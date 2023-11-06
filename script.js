52758f92c8f8ce302ba02634f5c27f559ca36de4
Sayantan Dey <137916304+SayantanDey29@users.noreply.github.com>
2023-11-06 17:29:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 /*------Menu icon navbar----- */
 let menuIcon=document.querySelector('#menu-icon');
 let navbar=document.querySelector('.navbar');
-
 menuIcon.onclick=()=>{
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
@@ -14,13 +13,11 @@ let section=document.querySelectorAll('section');
 let navlinks=document.querySelectorAll('header nav a');
 
 window.onscroll=()=>{
-
     section.forEach(sec=>{
         let top=window.scrollY;
         let offset=sec.offsetTop-150;
         let height=sec.offsetHeight;
         let id=sec.getAttribute('id');
-
         if(top>=offset && top<offset+height){
             navlinks.forEach(links=>{
                 links.classList.remove('active');
@@ -32,13 +29,11 @@ window.onscroll=()=>{
 
 /*------Sticky navbar----- */
 let header=document.querySelector('.header');
-
 header.classList.toggle('sticky',window.scrollY>100);
 
 /*---Remove Menu icon navbar--- */
 menuIcon.classList.remove('bx-x');
 navbar.classList.remove('active');
-
 
 };
 
